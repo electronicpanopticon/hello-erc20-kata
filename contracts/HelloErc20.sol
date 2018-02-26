ffe4d4bd4424a825d6d78b0f8e552d3b721bd2d2
Christoph <ignatz@gmail.com>
2018-02-26 11:50:29 -0500

    1.01 Added name

--- contracts/HelloErc20.sol
+++ contracts/HelloErc20.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.4;
 
 contract HelloErc20 {
+
+  string public name = "Hello ERC20 Coin";
+
   function HelloErc20() public {
     // constructor
   }
--- test/hello_erc20.js
+++ test/hello_erc20.js
@@ -1,9 +1,15 @@
+const mlog = require('mocha-logger');
+const util = require('util');
+require('chai').should();
+
 const HelloErc20 = artifacts.require('HelloERC20');
 
-contract('HelloErc20', function(accounts) {
-  it("should assert true", function(done) {
-    var hello_erc20 = HelloErc20.deployed();
-    assert.isTrue(true);
-    done();
+contract('HelloERC20', ([owner]) => {
+  describe('Given that I have a Token Contract', () => {
+    it('it should have the correct name', async () => {
+      const hello_erc20 = await HelloErc20.new({ from: owner });
+      const name = await hello_erc20.name();
+      name.should.be.equal("Hello ERC20 Coin");
+    });
   });
 });
